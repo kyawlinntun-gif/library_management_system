9f7fc462b935a761bdc3b282f887a1f447f58af5
kyawlinntun <59950395+kyawlinntun-gif@users.noreply.github.com>
2025-01-06 13:45:25 +0630

    Add register, login, and logout & admin template

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -11,9 +11,48 @@
         <li><a href="details.html">Item Details</a></li>
         <li><a href="author.html">Author</a></li>
         <li><a href="create.html">Create Yours</a></li>
+        @if(Route::has('login'))
+            @auth
+            <li><a href="#" onclick="event.preventDefault();document.getElementById('logout').submit();">Logout</a></li>
+            <form action="{{ route('logout') }}" method="POST" id="logout">
+                @csrf
+            </form>
+            @else
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('register') }}">Register</a></li>
+            @endauth
+        @endif
     </ul>   
     <a class='menu-trigger'>
         <span>Menu</span>
     </a>
     <!-- ***** Menu End ***** -->
 </nav>
+{{-- @if (Route::has('login'))
+                            <nav class="-mx-3 flex flex-1 justify-end">
+                                @auth
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                                    >
+                                        Dashboard
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                                    >
+                                        Log in
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                                        >
+                                            Register
+                                        </a>
+                                    @endif
+                                @endauth
+                            </nav>
+                        @endif --}}
